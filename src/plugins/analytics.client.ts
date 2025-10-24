a9b1e3bf638634177c31188d187fe1321c1a9d81
Gytisla <glazaitis@gmail.com>
2025-10-24 20:20:53 +0200

    google analytics

--- src/plugins/analytics.client.ts
+++ src/plugins/analytics.client.ts
@@ -29,35 +29,7 @@ export default defineNuxtPlugin(() => {
   }
   window.gtag = gtag
 
-  // Set default consent to 'denied' initially
-  gtag('consent', 'default', {
-    analytics_storage: 'denied',
-    functionality_storage: 'denied',
-    personalization_storage: 'denied',
-    security_storage: 'granted'
-  })
-
   // Configure Google Analytics
   gtag('js', new Date())
-  gtag('config', GA_ID, {
-    anonymize_ip: true,
-    allow_google_signals: false,
-    allow_ad_features: false
-  })
-
-  // Check for existing cookie consent
-  const cookieConsent = localStorage.getItem('toplay_cookie_consent')
-  if (cookieConsent) {
-    const settings = JSON.parse(cookieConsent)
-    if (settings.analytics) {
-      gtag('consent', 'update', {
-        analytics_storage: 'granted'
-      })
-    }
-    if (settings.functional) {
-      gtag('consent', 'update', {
-        functionality_storage: 'granted'
-      })
-    }
-  }
+  gtag('config', GA_ID)
 })
